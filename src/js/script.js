9625259961d6d05af93b676a7dc887bfd766e932
Adesh Katiya <adeshkatiya27@gmail.com>
2025-06-11 17:23:28 +0530

    style: toggling classes to show different fields on each radio button click

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,33 @@ const metricRadioBtn = document.getElementById("metric");
 const imperialRadioBtn = document.getElementById("imperial");
 
 const biometrics = document.getElementById("biometrics");
-const biometricFields = biometrics.querySelectorAll(".calculator__form-biometric");
+const metricFields = biometrics.querySelectorAll(".calculator__form-biometric");
+const imperialFields = biometrics.querySelectorAll(".calculator__form-fieldset");
 
 imperialRadioBtn.addEventListener("change", () => {
     console.log("changed");
-    biometrics.classList.toggle("calculator__form-metrics--metric");
-    biometrics.classList.toggle("calculator__form-metrics--imperial");
-    biometricFields.style.display = "none";
+    biometrics.classList.remove("calculator__form-biometrics--metric");
+
+    biometrics.classList.add("calculator__form-biometrics--imperial");
+    metricFields.forEach(field => {
+        field.style.display = "none";
+    });
+    imperialFields.forEach(field => {
+        field.style.display = "block";
+    });
 });
+
+metricRadioBtn.addEventListener("change", () => {
+    console.log("changed");
+    biometrics.classList.add("calculator__form-biometrics--metric");
+    biometrics.classList.remove("calculator__form-biometrics--imperial");
+    metricFields.forEach(field => {
+        field.style.display = "block";
+    });
+    imperialFields.forEach(field => {
+        field.style.display = "none";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => { metricRadioBtn.checked = "true" });
+
